e4694302d6a3786c64d954e0b3cf42786283bd3c
Anatol Pomazau <anatol@google.com>
2011-02-01 11:27:32 -0800

    Remove files that we do not use during compilation.
    This makes build script simpler. Also it will be easier to convert
    the buildfile to other build tools (such as Tup).
    
    Change-Id: I2d83d2d0c638bec50411e07d693cff3049f67903

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_SRC_FILES := $(filter-out %/JDiffAntTask.java,$(LOCAL_SRC_FILES))
 
 LOCAL_CLASSPATH := $(HOST_JDK_TOOLS_JAR)
 
