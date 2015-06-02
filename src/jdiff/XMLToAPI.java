2f5f31a8f36d12a0cbf4520f213ef325c18d40b3
Paul Rashidi <paulrashidi@google.com>
2015-06-02 14:58:19 -0700

    Removing the use of an external xml parsing lib.
    API JDiffs for Google Play services ran across a bug with the
    xerces.jar so switching to the native library to resolve.
    
    Change-Id: Ia5f8acbbeff54af51986512505933e83f7919d73

--- src/jdiff/XMLToAPI.java
+++ src/jdiff/XMLToAPI.java
@@ -2,6 +2,7 @@ package jdiff;
 
 import java.io.*;
 import java.util.*;
+import javax.xml.parsers.ParserConfigurationException;
 
 /* For SAX parsing in APIHandler */
 import org.xml.sax.Attributes;
@@ -47,19 +48,17 @@ public class XMLToAPI {
             XMLReader parser = null;
             DefaultHandler handler = new APIHandler(api_, createGlobalComments);
             try {
-                String parserName = System.getProperty("org.xml.sax.driver");
-                if (parserName == null) {
-                    parser = org.xml.sax.helpers.XMLReaderFactory.createXMLReader("org.apache.xerces.parsers.SAXParser");
-                } else {
-                    // Let the underlying mechanisms try to work out which 
-                    // class to instantiate
-                    parser = org.xml.sax.helpers.XMLReaderFactory.createXMLReader();
-                }
+                parser = javax.xml.parsers.SAXParserFactory.newInstance().newSAXParser().getXMLReader();
             } catch (SAXException saxe) {
                 System.out.println("SAXException: " + saxe);
                 saxe.printStackTrace();
                 System.exit(1);
+            } catch (ParserConfigurationException pce) {
+                System.out.println("ParserConfigurationException: " + pce);
+                pce.printStackTrace();
+                System.exit(1);
             }
+
             if (validateXML) {
                 parser.setFeature("http://xml.org/sax/features/namespaces", true);
                 parser.setFeature("http://xml.org/sax/features/validation", true);
